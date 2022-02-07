e034ee0636699bf5e71c8f0f721213023bc7676f
Liu Cong <innerr@gmail.com>
2022-02-07 15:37:40 +0800

    better args handling of cmd.vim*
    Signed-off-by: Liu Cong <innerr@gmail.com>

---
+++ cmd/vim/helper.bash
@@ -0,0 +1,36 @@
+function vim_path_from_api_get()
+{
+	local key="${1}"
+	local cmd="${2}"
+	local err_when_not_found=`to_true "${3}"`
+
+	local self=`must_env_val "${env}" 'sys.paths.ticat'`
+
+	set +e
+	local path=`"${self}" "${key}" "${cmd}"`
+	if [ $? != 0 ]; then
+		echo "[:(] get cmd path failed, wrong cmd?" >&2
+		exit 1
+	fi
+	set -e
+
+	if [ -z "${path}" ]; then
+		if [ "${err_when_not_found}" == 'true' ]; then
+			echo "[:(] cmd script/meta file not found, wrong cmd or builtin cmd?" >&2
+			exit 1
+		else
+			return 0
+		fi
+	fi
+
+	local lines=`echo "${path}" | wc -l`
+	if [ "${lines}" != 1 ]; then
+		echo "[:(] get cmd path failed, too many result lines:" >&2
+		echo "${path}" >&2
+		exit 1
+	fi
+	echo "[:)] editing ${path}"
+	vim "${path}"
+	echo "[:)] edited ${path}"
+	exit
+}
--- cmd/vim/meta.bash
+++ cmd/vim/meta.bash
@@ -1,5 +1,6 @@
 set -euo pipefail
 . "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../helper/helper.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/helper.bash"
 
 env=`cat "${1}/env"`
 shift
@@ -10,30 +11,5 @@ if [ -z "${cmd}" ]; then
 	exit 1
 fi
 
-self=`must_env_val "${env}" 'sys.paths.ticat'`
-
-set +e
-path=`"${self}" 'api.cmd.meta' "${cmd}"`
-if [ $? != 0 ]; then
-	echo "[:(] get cmd path failed, wrong cmd?" >&2
-	exit 1
-fi
-set -e
-if [ ! -z "${path}" ]; then
-	echo "[:)] editing ${path}"
-	vim "${path}"
-	exit
-fi
-
-set +e
-path=`"${self}" 'api.cmd.path' "${cmd}"`
-if [ $? != 0 ]; then
-	echo "[:(] get cmd path failed, wrong cmd?" >&2
-	exit 1
-fi
-set -e
-if [ ! -z "${path}" ]; then
-	echo "[:)] editing ${path}"
-	vim "${path}"
-	exit
-fi
+vim_path_from_api_get 'api.cmd.meta' "${cmd}" 'false'
+vim_path_from_api_get 'api.cmd.path' "${cmd}" 'true'
