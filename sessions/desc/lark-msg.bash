b50496b6e84ff730d7115e7d98eefe02367b614c
Liu Cong <innerr@gmail.com>
2022-06-22 22:11:36 +0800

    send a remedy notification to lark if gathered-info not sent
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- sessions/desc/lark-msg.bash
+++ sessions/desc/lark-msg.bash
@@ -10,7 +10,7 @@ url=`must_env_val "${env}" 'lark.secret.notify-url'`
 
 set +e
 msg=`"${ticat}" {sys.session.disable=true} display.quiet : display.tips.off : display.plain : \
-	echo "[${session}] - [[sys.session.id.ip]]" : sessions.desc "${session}" 3 2>&1`
+	echo "[${session}] - [[sys.session.id.ip]]" : sessions.desc "${session}" 2 2>&1`
 set -e
 
 if [ -z "${msg}" ]; then
@@ -24,10 +24,29 @@ msg_prefix='{"msg_type":"text","content":{"text":"'
 msg_suffix='"}}'
 msg="${msg_prefix}${msg}${msg_suffix}"
 
-result=`curl -s -X POST -H "Content-Type: application/json" -d "${msg}" "${url}" | awk '{print $0}'`
+set +e
+result=`curl -s -X POST -H "Content-Type: application/json" -d "${msg}" "${url}" 2>&1`
+set -e
 ok=`echo "${result}" | { grep 'success' || test $? = 1; } `
-if [ -z "${ok}" ]; then
-	echo "[:(] message sent to lark failed: ${result}" >&2
-else
+
+if [ ! -z "${ok}" ]; then
 	echo "[:)] message sent to lark"
+	exit
+fi
+
+echo "******" >&2
+echo "${msg}" >&2
+echo "******" >&2
+echo "[:(] message sent to lark failed: ${result}" >&2
+
+echo "[:-] remedy message sending" >&2
+remedy_msg="${msg_prefix}ticat session exited, but sending gathered-info to lark failed, plz check the log${msg_suffix}"
+
+set +e
+curl -s -X POST -H "Content-Type: application/json" -d "${remedy_msg}" "${url}" 2>&1 | awk '{print "[:-] "$0}'
+ret=${?}
+set -e
+
+if [ ${ret} == 0 ]; then
+	echo "[:-] remedy message sent" >&2
 fi
